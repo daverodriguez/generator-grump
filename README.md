ae70a3599e0b42daeba05eab699fe3a5c4095bb1
Dave Rodriguez <dave.rodriguez@hansoninc.com>
2015-05-06 17:29:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+![Grump-grump](assets/grump-grump-logo.png)
+
+---------------------
+
+Grump-grump is a [Yeoman](http://yeoman.io) generator. It installs all the Grunt plugins you need for simple build projects,
+including:
+
+- grunt-contrib-connect
+- grunt-contrib-watch
+- grunt-bake
+- grunt-contrib less
+- grunt-contrib-uglify
+- grunt-contrib-copy
+- grunt-imagemin
+
+Grump-grump also preconfigures your plugins in Gruntfile.js to get you started more quickly!
+
+## Instructions
+To install Grump-grump from npm, open up a console and run:
+
+```
+npm install -g yo generator-grump
+```
+
+Finally, initiate the generator with `yo grump`.
+
+Once everything is installed, run `grunt` to build the project and `grunt connect` to start a local web server.
+
+## License
+
+Grump-grump is distributed under the MIT license.
---
+++ app/index.js
@@ -0,0 +1,230 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+var slash = require('slash')
+
+var GrumpGenerator = yeoman.generators.Base.extend({
+	initializing: function () {
+		this.pkg = require('../package.json');
+	},
+
+	setupRoot: function() {
+		var hbsOptions = {
+			nested: false,
+			appType: 'standard',
+			appName: this.appName,
+			appNS: this.appNS,
+			jsPath: slash(path.join(this.srcDir, this.jsPath)),
+			srcDir: this.srcDir || null,
+			buildDir: this.buildDir || null,
+			connectPort: 3000,
+			createController: true,
+			controllerName: 'home',
+			createExampleClasses: false
+		};
+
+		this.composeWith( 'hanbs:standard-app', {
+			arguments: ['grump'],
+			options: hbsOptions
+		} );
+	},
+
+	prompting: function () {
+		var done = this.async();
+
+		// Have Yeoman greet the user.
+		this.log( yosay('Welcome to Grump-Grump!' ) );
+
+		var prompts = [
+			{
+				name: 'appName',
+				message: 'What\'s the name of your application?',
+				default: 'Example Grump Application'
+			},
+			{
+				name: 'appNS',
+				message: 'What\'s the namespace of your app?',
+				default: 'APP'
+			},
+			{
+				name: 'srcDir',
+				message: 'Where is your source directory?',
+				default: 'src'
+			},
+			{
+				name: 'jsPath',
+				message: 'What is the path to your JavaScript folder (inside the source folder)?',
+				default: 'js'
+			},
+			{
+				name: 'imgPath',
+				message: 'What is the path to your image folder (inside the source folder)?',
+				default: 'img'
+			},
+			{
+				name: 'stylePath',
+				message: 'What is the path to your stylesheet folder (inside the source folder)?',
+				default: 'less'
+			},
+			{
+				name: 'compiledStylePath',
+				message: 'What is the path to your compiled stylesheet folder (inside the build folder)?',
+				default: 'css'
+			},
+			{
+				name: 'fontsPath',
+				message: 'What is the path to your fonts folder (inside the source folder)?',
+				default: 'fonts'
+			},
+			{
+				name: 'buildDir',
+				message: 'Where is your build directory?',
+				default: 'build'
+			},
+			{
+				type: 'input',
+				name: 'connectPort',
+				options: {
+					type: 'number'
+				},
+				message: 'What port would you like grunt-connect web server to run on?',
+				default: 3000
+			},
+			{
+				type: 'confirm',
+				name: 'includeCompress',
+				message: 'Do you want a task to generate a ZIP file of your project?',
+				default: true
+			},
+			{
+				type: 'confirm',
+				name: 'includeFileindex',
+				message: 'Do you want to generate an index file at the root of your project?',
+				default: true
+			}
+		];
+
+		this.prompt(prompts, function (props) {
+			this.includeCompress = props.includeCompress;
+			this.includeFileindex = props.includeFileindex;
+			this.connectPort = props.connectPort;
+			this.appName = props.appName;
+			this.appNS = props.appNS;
+			this.jsPath = props.jsPath;
+			this.imgPath = props.imgPath;
+			this.stylePath = props.stylePath;
+			this.compiledStylePath = props.compiledStylePath;
+			this.fontsPath = props.fontsPath;
+			this.srcDir = props.srcDir;
+			this.buildDir = props.buildDir;
+			this.connectPort = props.connectPort;
+
+			done();
+		}.bind(this));
+
+
+	},
+
+	writing: {
+		app: function () {
+			this.src.copy('_package.json', 'package.json');
+			this.src.copy('_bower.json', 'bower.json');
+		},
+
+		projectfiles: function () {
+			this.src.copy('editorconfig', '.editorconfig');
+			this.src.copy('jshintrc', '.jshintrc');
+		},
+
+		gruntfile: function() {
+			// Add gruntfile configs
+			this.gruntfile.insertConfig('bake', "{ default: { files: { '" + slash(path.join(this.buildDir, '/index.html') + "': '" + path.join(this.srcDir, '/index.html') + "' } } }"));
+			this.gruntfile.insertConfig('connect', "{ server: { options: { port: " + this.connectPort + ", keepalive: true, livereload: true, base : '" + this.buildDir + "', hostname: '*' } } }");
+			this.gruntfile.insertConfig('copy', "{ js: { files: [ { expand: true, cwd: '" + slash(path.join(this.srcDir, this.jsPath)) + "', src: ['**'], dest: '" + slash(path.join(this.buildDir, this.jsPath)) + "' } ] }, img: { files: [ { expand: true, cwd: '" + slash(path.join(this.srcDir, this.imgPath)) + "', src: ['**'], dest: '" + slash(path.join(this.buildDir, this.imgPath)) + "' } ] }, fonts: { files: [ { expand: true, cwd: '" + slash(path.join(this.srcDir, this.fontsPath)) + "', src: ['**'], dest: '" + slash(path.join(this.buildDir, this.fontsPath)) + "' } ] } }");
+			this.gruntfile.insertConfig('imagemin', "{ png: { options: { optimizationLevel: 7 }, files: [ { expand: true, cwd: '"+ slash(path.join(this.srcDir, this.imgPath)) + "', src: ['**/*.{png,jpg}'], dest: '"+ slash(path.join(this.srcDir, this.imgPath)) + "' } ] } }");
+			this.gruntfile.insertConfig('less', "{ default: { files: { '" + slash(path.join(this.buildDir, this.compiledStylePath, 'app.css')) + "': '" + slash(path.join(this.srcDir, this.stylePath, 'app.less')) + "' } } }");
+			this.gruntfile.insertConfig('uglify', "{ js: { files: { '" + slash(path.join(this.buildDir, this.jsPath, 'libs.js')) + "' : ['" + slash(path.join(this.buildDir, this.jsPath, 'lib/hbs.js')) + "'] } } }");
+			this.gruntfile.insertConfig('watch', "{ less: { files: ['" + slash(path.join(this.srcDir, '**/*.less')) + "'], tasks: ['less'], options: { livereload: true } }, html: { files: ['" + slash(path.join(this.srcDir, '**/*.html')) + "'], tasks: ['bake'], options: { livereload: true } }, js: { files: ['" + slash(path.join(this.srcDir, '**/*.js')) + "'], tasks: ['copy', 'uglify'], options: { livereload: true } }, img: { files: ['" + slash(path.join(this.srcDir, '**/*.{png,gif,jpg}')) + "'], tasks: ['copy:img', 'imagemin'] } }");
+
+			// Optional task configs
+			if (this.includeCompress) {
+				this.gruntfile.insertConfig('compress', "{ default: { options: { archive: 'project.zip' }, files: [ {src: ['" + slash(path.join(this.buildDir, '**')) + "'], dest: '/'}, {src: ['docs/**'], dest: '/'}, {src: ['" + slash(path.join(this.srcDir, '**')) + "'], dest: '/'}, {src: ['Gruntfile.js'], dest: '/'}, {src: ['package.json'], dest: '/'} ] } }");
+			}
+
+			if (this.includeFileindex) {
+				var fiConfig = "{\r\n\tcustom: {\r\n\t\t\t\toptions: {\r\n\t\t\t\t\tformat: function (list, options, dest) {\r\n\t\t\t\t\t\tvar html = \'<html><head><meta name=\"viewport\" content=\"width=device-width;\" \/><\/head><body>\';\r\n\t\t\t\t\t\thtml = html.concat(\'<h1>Template Listing<\/h1>\\n\\n<ul>\\n\');\r\n\t\t\t\t\t\tfor (var i in list) {\r\n\t\t\t\t\t\t\tif (!list.hasOwnProperty(i)) { continue; }\r\n\t\t\t\t\t\t\tvar next = list[i];\r\n\t\t\t\t\t\t\thtml = html.concat(\'<li><a href=\"\' + next + \'\">\' + next + \'<\/a><\/li>\\n\');\r\n\t\t\t\t\t\t}\r\n\t\t\t\t\t\thtml = html.concat(\'<\/ul>\');\r\n\t\t\t\t\t\thtml = html.concat(\'<\/body><\/html>\');\r\n\r\n\t\t\t\t\t\treturn html;\r\n\t\t\t\t\t}\r\n\t\t\t\t},\r\n\t\t\t\tfiles: [\r\n\t\t\t\t\t{dest: '" + slash(path.join(this.buildDir, 'index.html')) + "', src: [\'*.html\'], cwd: '" + this.srcDir + "', filter: \'isFile\'}\r\n\t\t\t\t]\t\r\n\t\t\t}\r\n}";
+				this.gruntfile.insertConfig('fileindex', fiConfig);
+			}
+
+			// Load NPM tasks
+			this.gruntfile.loadNpmTasks('grunt-bake');
+			this.gruntfile.loadNpmTasks('grunt-contrib-concat');
+			this.gruntfile.loadNpmTasks('grunt-contrib-connect');
+			this.gruntfile.loadNpmTasks('grunt-contrib-copy');
+			this.gruntfile.loadNpmTasks('grunt-contrib-imagemin');
+			this.gruntfile.loadNpmTasks('grunt-contrib-less');
+			this.gruntfile.loadNpmTasks('grunt-contrib-uglify');
+			this.gruntfile.loadNpmTasks('grunt-contrib-watch');
+
+			// Load optional NPM tasks
+			if (this.includeCompress) {
+				this.gruntfile.loadNpmTasks('grunt-contrib-compress');
+			}
+
+			if (this.includeFileindex) {
+				this.gruntfile.loadNpmTasks('grunt-fileindex');
+			}
+
+			// Register default tasks
+			var defaultTasks = ['less', 'bake', 'concat', 'uglify', 'copy'];
+
+			// Optional task
+			if (this.includeFileindex) {
+				defaultTasks.push('fileindex')
+			}
+
+			this.gruntfile.registerTask('default', defaultTasks);
+
+			// Optional task
+			if (this.includeCompress) {
+				this.gruntfile.registerTask('compress', ['compress']);
+			}
+		}
+	},
+
+	install: function () {
+		// Default dependencies
+		var deps = [
+			'grunt',
+			'grunt-bake',
+			'grunt-contrib-concat',
+			'grunt-contrib-connect',
+			'grunt-contrib-copy',
+			'grunt-contrib-imagemin',
+			'grunt-contrib-less',
+			'grunt-contrib-uglify',
+			'grunt-contrib-watch'
+		];
+
+		// Optional dependencies
+		if (this.includeCompress) {
+			deps.push('grunt-contrib-compress')
+		}
+
+		if (this.includeFileindex) {
+			deps.push('grunt-fileindex');
+		}
+
+		this.npmInstall( deps, {'saveDev': true}, null );
+
+
+	},
+
+	end: function () {
+		this.installDependencies();
+	}
+});
+
+module.exports = GrumpGenerator;
